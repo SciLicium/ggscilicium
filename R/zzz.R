7c9d5058e69f9f110473248905e16894887fa381
SciLiciumTheo <101104633+SciLiciumTheo@users.noreply.github.com>
2022-06-27 10:10:33 +0200

    Update library startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,21 @@
 #' @export
 #' @import sysfonts
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("A bunch of SciLicium themes and colors!")
+  version = packageDescription(pkgname, fields = "Version")
+  
+  msg = paste0("========================================
+", pkgname, " version ", version, "
+A bunch of SciLicium themes and colors!
 
+Github page: https://github.com/SciLicium/ggscilicium
+
+This message can be suppressed by:
+  suppressPackageStartupMessages(library(circlize))
+========================================
+")
+  
+  packageStartupMessage(msg)
+  
   if(!("titillium" %in% sysfonts::font_families())){
     sysfonts::font_add_google("Titillium Web", "titillium")
   }
