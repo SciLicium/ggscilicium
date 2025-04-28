31f9ef27a1f0d7a0095b57c88fd1ac2282c4ed5e
SciLiciumTheo <101104633+SciLiciumTheo@users.noreply.github.com>
2025-04-28 16:36:31 +0200

    FIX add fonts to the package
    sysfonts add fonts only temporarily https://github.com/yixuan/sysfonts/issues/12
    This commit adds the fonts to the package, to avoid downloading the fonts each time the library is loaded.

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
 #' @import utils
 .onLoad <- function(libname, pkgname) {
   version = packageDescription(pkgname, fields = "Version")
-  
+
   msg = paste0("========================================
 ", pkgname, " version ", version, "
 A bunch of SciLicium themes and colors!
@@ -17,11 +17,18 @@ This message can be suppressed by:
   suppressPackageStartupMessages(library(ggscilicium))
 ========================================
 ")
-  
+
   packageStartupMessage(msg)
-  
+
   if(!("titillium" %in% sysfonts::font_families())){
-    sysfonts::font_add_google("Titillium Web", "titillium")
+    #sysfonts::font_add_google("Titillium Web", "titillium")
+    #sysfonts::font_paths("fonts/TitilliumWeb")
+    sysfonts::font_paths(system.file("fonts/TitilliumWeb", package = pkgname))
+    sysfonts::font_add("titillium",
+                       "TitilliumWeb-Regular.ttf",
+                       "TitilliumWeb-Bold.ttf",
+                       "TitilliumWeb-Italic.ttf",
+                       "TitilliumWeb-BoldItalic.ttf")
   }
   showtext::showtext_auto()
 }
